3d812c3c2bbd4a2c216bdf1a5e8fc33c96c1e3a0
bogumydoll99 <bogumydoll99@gmail.com>
2026-08-05 21:49:07 +0100

    Update novels.js

--- js/data/novels.js
+++ js/data/novels.js
@@ -1,1 +1,25 @@
+const novels = [
+    {
+        title: "Shadow Emperor",
+        genre: "Fantasy • Action • Adventure",
+        image: "https://picsum.photos/300/420?1"
+    },
 
+    {
+        title: "Rebirth of Queen",
+        genre: "Romance • Revenge",
+        image: "https://picsum.photos/300/420?2"
+    },
+
+    {
+        title: "Legend of Sword",
+        genre: "Martial Arts",
+        image: "https://picsum.photos/300/420?3"
+    },
+
+    {
+        title: "The Second Wife Just Wants to Tiger-Mom Her Stepson",
+        genre: "Comedy • Drama • Fantasy • Historical • Romance",
+        image: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQIyr1Misto6RU5pHQWe1TMLjKcm5yirQSL8SN8SElllUvUqhQ7"
+    }
+];
