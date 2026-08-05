65dd3aa07307278ee443c04021381e0ac336465f
bogumydoll99 <bogumydoll99@gmail.com>
2026-08-05 21:08:19 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
-function openNovel() {
-    alert("Opening the novel...");
-}
+let novelName = "The Second Wife Just Wants to Tiger-Mom Her Stepson";
+let chapters = 489;
+
+alert("Novel: " + novelName);
+alert("Chapters: " + chapters);
