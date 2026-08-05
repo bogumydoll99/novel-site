3dcc16be7a8428083d61c964b3412dd7fd511215
bogumydoll99 <bogumydoll99@gmail.com>
2026-08-05 21:23:24 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ let novels = [
     "Rebirth of Queen",
     "Legend of Sword",
     "The Second Wife Just Wants to Tiger-Mom Her Stepson"
+    "Solo Leveling"
 ];
 
 alert(novels[0]);
 alert(novels[1]);
 alert(novels[2]);
 alert(novels[3]);
+alert(novels[4]);
