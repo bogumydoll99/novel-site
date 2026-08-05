e83da25c3882785bfba115d1c4a6009a1e5ea499
bogumydoll99 <bogumydoll99@gmail.com>
2026-08-05 21:36:54 +0100

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,11 @@
+let novel = {
+    title: "The Second Wife Just Wants to Tiger-Mom Her Stepson",
+    author: "Dudu Cutou",
+    genre: "Historical • Romance • Comedy",
+    chapters: 489
+};
+
+alert(novel.title);
+alert(novel.author);
+alert(novel.genre);
+alert(novel.chapters);
