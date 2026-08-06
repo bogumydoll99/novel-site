370dd8f1bcb577eec55c5254adf760fa4f3e0b8d
bogumydoll99 <bogumydoll99@gmail.com>
2026-08-06 22:02:58 +0100

    Update novels.js

--- js/data/novels.js
+++ js/data/novels.js
@@ -1,29 +1,29 @@
 const novels = [
-    {
-        id: 1, 
-        title: "Shadow Emperor",
-        genre: "Fantasy • Action • Adventure",
-        image: "https://picsum.photos/300/420?1"
-    },
+{
+    id: 1,
+    title: "Shadow Emperor",
+    genre: "Fantasy • Action • Adventure",
+    image: "https://picsum.photos/300/420?1"
+},
 
-    {
-        id: 2,
-        title: "Rebirth of Queen",
-        genre: "Romance • Revenge",
-        image: "https://picsum.photos/300/420?2"
-    },
+{
+    id: 2,
+    title: "Rebirth of Queen",
+    genre: "Romance • Revenge",
+    image: "https://picsum.photos/300/420?2"
+},
 
-    {
-        id: 3,
-        title: "Legend of Sword",
-        genre: "Martial Arts",
-        image: "https://picsum.photos/300/420?3"
-    },
+{
+    id: 3,
+    title: "Legend of Sword",
+    genre: "Martial Arts",
+    image: "https://picsum.photos/300/420?3"
+},
 
-    {
-        id: 4,
-        title: "The Second Wife Just Wants to Tiger-Mom Her Stepson",
-        genre: "Comedy • Drama • Fantasy • Historical • Romance",
-        image: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQIyr1Misto6RU5pHQWe1TMLjKcm5yirQSL8SN8SElllUvUqhQ7"
-    }
+{
+    id: 4,
+    title: "The Second Wife Just Wants to Tiger-Mom Her Stepson",
+    genre: "Comedy • Drama • Fantasy • Historical • Romance",
+    image: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQIyr1Misto6RU5pHQWe1TMLjKcm5yirQSL8SN8SElllUvUqhQ7"
+}
 ];
