b442b925932c01264f8a23373831785f1fcfb21a
bogumydoll99 <bogumydoll99@gmail.com>
2026-08-06 21:37:57 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,14 +1,25 @@
 const container = document.getElementById("novels-container");
+const search = document.getElementById("search");
 
-novels.forEach(function(novel) {
+function displayNovels(list) {
 
-    container.innerHTML += `
-        <div class="card">
-            <img src="${novel.image}">
-            <div class="card-content">
-                <h3>${novel.title}</h3>
-                <p>${novel.genre}</p>
-                <a href="novel.html" class="read-btn">Read Now</a>
+    container.innerHTML = "";
+
+    list.forEach(function(novel) {
+
+        container.innerHTML += `
+            <div class="card">
+                <img src="${novel.image}">
+                <div class="card-content">
+                    <h3>${novel.title}</h3>
+                    <p>${novel.genre}</p>
+                    <a href="novel.html" class="read-btn">Read Now</a>
+                </div>
             </div>
-            </div>
-                     });
+        `;
+
+    });
+
+}
+
+displayNovels(novels);
